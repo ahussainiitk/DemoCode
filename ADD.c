e8bcc8c40e09ae450cf9ca1d374bd79af4ed6b9c
AHussain <ahussainiitk@gmail.com>
2023-04-27 00:59:50 +0800

    Arif

--- ADD.c
+++ ADD.c
@@ -2,7 +2,7 @@
 // Simple C program to display "Hello World"
   
 // Header file for input output functions
-#include <stdio.h>
+#include <stdio.h> 
   
 // main function -
 // where the execution of program begins
@@ -11,6 +11,12 @@ int main()
   
     // prints hello world
     printf("Hello World, This is Arif Hussain");
+    int a, b, c;
+
+    a = 2;
+    b = 3;
+    c = a+b;
+    printf("%d + %d = %d", a, b, c);
   
     return 0;
 }
